fdff75cdaca6d30c1125c988e8768466134780c7
dorinemoulin <dorinemoulin2@orange.fr>
2024-06-15 12:45:49 +0200

    Menu hbg

--- assets/js/script-nav.js
+++ assets/js/script-nav.js
@@ -1,23 +1,33 @@
-function sliders(_classes){
+// function sliders(_classes){
     
-const header = document.querySelector('header'),
-      hbg = document.querySelector('#hbg'),
-      as = header.querySelectorAll('nav > a:not(:last-child)');
+// const header = document.querySelector('header'),
+//       hbg = document.querySelector('#hbg'),
+//       as = header.querySelectorAll('nav > a:not(:last-child)');
 
-hbg.addEventListener('click', function(e) {
-    header.classList.toggle('open');
-});
+// hbg.addEventListener('click', function(e) {
+//     header.classList.toggle('open');
+// });
 
-function addEvents(_elems, _cb, _type = "click") {
-    document.querySelectorAll(_elems).forEach(el => {
-        el.addEventListener(_type, e => {
-            _cb(e);
-        });
+// function addEvents(_elems, _cb, _type = "click") {
+//     document.querySelectorAll(_elems).forEach(el => {
+//         el.addEventListener(_type, e => {
+//             _cb(e);
+//         });
+//     });
+// }
+
+// addEvents('.nofollow', e => {
+//     e.currentTarget.href = e.currentTarget.dataset.path;
+// });
+
+// }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const header = document.querySelector('header');
+    const hbg = document.querySelector('#hbg');
+
+    hbg.addEventListener('click', function (e) {
+        e.preventDefault(); // Empêche le comportement par défaut du lien
+        header.classList.toggle('open');
     });
-}
-
-addEvents('.nofollow', e => {
-    e.currentTarget.href = e.currentTarget.dataset.path;
 });
-
-}
